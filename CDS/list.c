3bffa4c39bb769f6377c4f23d7974857bd93cea5
pineforest2 <pine_forest_win@outlook.com>
2024-05-12 07:41:15 +0000

    headache

--- CDS/list.c
+++ CDS/list.c
@@ -67,6 +67,7 @@ void popStack(struct stack *s) {
 }
 void destroyStack(struct stack *s) { free(s); s = NULL; }
 
+
 /* 例程：用数组实现的顺序表。程序仅作参考。 */
 #define INIT_NUM 58 /* user-defined */
 struct vector {
@@ -125,6 +126,74 @@ void destroyVector(struct vector *v) {
 	free(v); v = NULL;
 }
 
+
+/* 例程：双向循环链表。程序仅作参考。 */
+struct LinkedListNode {
+	struct LinkedListNode *prev, *next;
+	int data;
+};
+struct LinkedList {
+	struct LinkedListNode *head;
+	size_t size;
+};
+struct LinkedList *initLinkedList() {
+	struct LinkedList *list = (struct LinkedList *)malloc(sizeof(struct LinkedList));
+	assert(list != NULL);
+	list->head = NULL;
+	list->size = 0;
+	return list;
+}
+struct LinkedListNode *makeLinkedListNode(int data) {
+	struct LinkedListNode *node =
+		(struct LinkedListNode *)malloc(sizeof(struct LinkedListNode));
+	assert(node != NULL);
+	node->prev = NULL;
+	node->next = NULL;
+	node->data = data;
+	return node;
+}
+void freeLinkedListNode(struct LinkedListNode **node) {
+	free(*node); *node = NULL;
+}
+void pushfrontLinkedList(struct LinkedList *list, int data) {
+	struct LinkedListNode *ptr = list->head;
+	list->head = makeLinkedListNode(data);
+	if (ptr != NULL) {
+		assert(list->size != 0);
+		ptr->prev->next = list->head;
+		list->head->prev = ptr->prev;
+		list->head->next = ptr;
+		ptr->prev = list->head;
+	} else {
+		list->head->next = list->head;
+		list->head->prev = list->head;
+	}
+	(list->size)++;
+}
+void popfrontLinkedList(struct LinkedList *list) {
+	assert(list->head != NULL && list->size != 0);
+	struct LinkedListNode *ptr = list->head;
+	if (list->size == 1) {
+		assert(ptr == ptr->next && ptr == ptr->prev);
+		freeLinkedListNode(&ptr);
+	} else {
+		list->head = ptr->next;
+		list->head->prev = ptr->prev;
+		ptr->prev->next = list->head;
+		freeLinkedListNode(&ptr);
+	}
+	(list->size)--;
+}
+
+void test1LinkedList() {
+	struct LinkedList *list = initLinkedList();
+	pushfrontLinkedList(list, 2);
+	popfrontLinkedList(list); // bug
+	pushfrontLinkedList(list, 7);
+	pushfrontLinkedList(list, 8);
+	popfrontLinkedList(list);
+}
+
 /* 简单的测试 */
 int main() {
 	struct queue *q = initQueue();
@@ -155,4 +224,6 @@ int main() {
 	for (int i = 0; i < v->length; ++i) printf("%d ", atVector(v, i));
 	putchar('\n');
 	destroyVector(v);
+
+	test1LinkedList();
 }
