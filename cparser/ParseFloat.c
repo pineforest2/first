cd11ef23156bd4c60c55353cd1d1aacb0ed2cc04
pineforest2 <pine_forest_win@outlook.com>
2024-06-02 10:02:59 +0000

    20240602

--- cparser/ParseFloat.c
+++ cparser/ParseFloat.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <assert.h>
+#include <math.h>
 
 #include "common.h"
 
 #define IS_DEC_DIGIT(ch) ((ch)>='0' && (ch)<='9')
+#define ABS(x) (((x) > 0) ? (x) : -(x))
 
 static inline int
 __ch2num(char ch) {
@@ -13,11 +15,18 @@ __ch2num(char ch) {
 	return -1;
 }
 
-static inline unsigned long
-__parse_dec_ulong(const char *str, unsigned int len) {
+static inline long
+__parse_dec_long(const char *str, unsigned int len) {
 	unsigned int idx = 0;
 	unsigned int ridx = len - 1;
 	int value = 0;
+	char isneg = 0;
+	if (len == 0) return 0;
+	if (*str == '+') idx++;
+	if (*str == '-') {
+		idx++;
+		isneg = 1;
+	}
 	/* processing leading zero */
 	while (idx < len) {
 		if (*(str + idx) == '0') {
@@ -32,7 +41,7 @@ __parse_dec_ulong(const char *str, unsigned int len) {
 		if (ridx > 0) ridx--;
 		else break;
 	}
-	return value;
+	return (isneg == 1) ? -value : value;
 }
 
 static inline void
@@ -40,7 +49,9 @@ __find_dot_eE_plus_minus(
 	const char *str, unsigned int len,
 	char *isneg1, char *isneg2,
 	unsigned int *dot_idx, unsigned int *e_idx) {
-	unsigned int idx;
+	unsigned int idx = 0;
+	if (*str == '-') *isneg1 = 1;
+	if (*str == '+') *isneg1 = 2;
 	while (idx < len) {
 		if (*(str + idx) == '.') {
 			assert(*dot_idx == 0xffffffff);
@@ -54,17 +65,15 @@ __find_dot_eE_plus_minus(
 			assert(*e_idx == 0xffffffff);
 			*e_idx = idx;
 		}
-		if (*(str + idx) == '+') {
-			assert(*isneg1 == 0 || *isneg2 == 0);
-			assert(idx == 0 || (*e_idx != 0xffffffff && *e_idx+1 == idx));
-			if (*isneg1 == 0) *isneg1 = 2;
-			else *isneg2 = 2;
+		if (*(str + idx) == '+' && idx != 0) {
+			assert(*isneg2 == 0);
+			assert(*e_idx != 0xffffffff && *e_idx+1 == idx);
+			*isneg2 = 2;
 		}
-		if (*(str + idx) == '-') {
-			assert(*isneg1 == 0 || *isneg2 == 0);
-			assert(idx == 0 || (*e_idx != 0xffffffff && *e_idx+1 == idx));
-			if (*isneg1 == 0) *isneg1 = 1;
-			else *isneg2 = 1;
+		if (*(str + idx) == '-' && idx != 0) {
+			assert(*isneg2 == 0);
+			assert(*e_idx != 0xffffffff && *e_idx+1 == idx);
+			*isneg2 = 1;
 		}
 		idx++;
 	}
@@ -84,63 +93,26 @@ float parse_float(const char *str, unsigned int len) {
 
 	if (dot_idx != 0xffffffff && e_idx != 0xffffffff) {
 		/*  str[0:len - 1] -> n1, n2, n3 -> value  */
-		if (isneg1 == 0)
-			n1 = __parse_dec_ulong(str, dot_idx);
-		else if (isneg1 == 1)
-			n1 = -__parse_dec_ulong(str + 1, dot_idx - 1);
-		else if (isneg1 == 2)
-			n1 = __parse_dec_ulong(str + 1, dot_idx - 1);
-		else
-			assert(0);
-		n2 = __parse_dec_ulong(str + dot_idx + 1, e_idx - dot_idx - 1);
-		n2 = (isneg1 == 1) ? -n2 : n2;
-		if (isneg2 == 0)
-			n3 = __parse_dec_ulong(str + e_idx + 1, len - e_idx - 1);
-		else if (isneg2 == 1)
-			n3 = -__parse_dec_ulong(str + e_idx + 2, len - e_idx - 2);
-		else if (isneg2 == 2)
-			n3 = __parse_dec_ulong(str + e_idx + 2, len - e_idx - 2);
-		else
-			assert(0);
-		n3 = (isneg2 == 1) ? -n3 : n3;
-		value = (n1 + fast_pow2(10, e_idx - dot_idx - 1) * n2) * fast_pow1(10, n3);
+		n1 = ABS(__parse_dec_long(str, dot_idx));
+		n2 = __parse_dec_long(str + dot_idx + 1, e_idx - dot_idx - 1);
+		n3 = __parse_dec_long(str + e_idx + 1, len - e_idx - 1);
+		value = (n1 + fast_pow2(10, -(e_idx - dot_idx - 1)) * n2) * fast_pow2(10, n3);
+		value = (isneg1 == 1) ? -value : value;
 	} else if (dot_idx == 0xffffffff && e_idx != 0xffffffff) {
 		/*  str[0:len - 1] -> n1, n2 -> value  */
-		if (isneg1 == 0)
-			n1 = __parse_dec_ulong(str, e_idx);
-		else if (isneg1 == 1)
-			n1 = -__parse_dec_ulong(str + 1, e_idx - 1);
-		else if (isneg1 == 2)
-			n1 = __parse_dec_ulong(str + 1, e_idx - 1);
-		else
-			assert(0);
-		n2 = __parse_dec_ulong(str + e_idx + 1, len - e_idx - 1);
-		n2 = (isneg1 == 1) ? -n2 : n2;
-		value = (float) n1 * fast_pow1(10, n2);
+		n1 = __parse_dec_long(str, e_idx);
+		n2 = __parse_dec_long(str + e_idx + 1, len - e_idx - 1);
+		value = (float) n1 * fast_pow2(10, n2);
 	} else if (dot_idx != 0xffffffff && e_idx == 0xffffffff) {
 		/*  str[0:len - 1] -> n1, n2 -> value  */
-		if (isneg1 == 0)
-			n1 = __parse_dec_ulong(str, dot_idx);
-		else if (isneg1 == 1)
-			n1 = -__parse_dec_ulong(str + 1, dot_idx - 1);
-		else if (isneg1 == 2)
-			n1 = __parse_dec_ulong(str + 1, dot_idx - 1);
-		else
-			assert(0);
-		n2 = __parse_dec_ulong(str + dot_idx + 1, len - dot_idx - 1);
-		n2 = (isneg1 == 1) ? -n2 : n2;
+		n1 = ABS(__parse_dec_long(str, dot_idx));
+		n2 = __parse_dec_long(str + dot_idx + 1, len - dot_idx - 1);
 		value = n1 + fast_pow2(10, -(len - dot_idx - 1)) * n2;
+		value = (isneg1 == 1) ? -value : value;
 	} else {
 		assert(isneg2 == 0);
 		/*  str[0:len - 1] -> n1 -> value  */
-		if (isneg1 == 0)
-			n1 = __parse_dec_ulong(str, len);
-		else if (isneg1 == 1)
-			n1 = -__parse_dec_ulong(str + 1, len - 1);
-		else if (isneg1 == 2)
-			n1 = __parse_dec_ulong(str + 1, len - 1);
-		else
-			assert(0);
+		n1 = __parse_dec_long(str, len);
 		value = (float) n1;
 	}
 	return value;
@@ -148,10 +120,9 @@ float parse_float(const char *str, unsigned int len) {
 
 #ifndef IS_LIB
 
-#define FLOAT_EQ(x, y) ((x) - (y) < 1e-6 || (y) - (x) < 1e-6)
 #define TEST_FLOAT_EQ(x, y) \
 	do { \
-		if (x - y < 1e-6 || y - x < 1e-6) printf("passed\n"); \
+		if (ABS((x) - (y)) < 1e-6) printf("passed\n"); \
 		else printf("error\n"); \
 	} while (0)
 
@@ -161,5 +132,13 @@ int main() {
 	TEST_FLOAT_EQ(-1.1, parse_float("-1.1", 4));
 	TEST_FLOAT_EQ(001.1, parse_float("001.1", 5));
 	TEST_FLOAT_EQ(-001.1, parse_float("-001.1", 6));
+	TEST_FLOAT_EQ(1., parse_float("1.", 2));
+	TEST_FLOAT_EQ(.1, parse_float(".1", 2));
+	TEST_FLOAT_EQ(11e-1, parse_float("11e-1", 5));
+	TEST_FLOAT_EQ(11E-1, parse_float("11E-1", 5));
+	TEST_FLOAT_EQ(+11e-1, parse_float("+11e-1", 6));
+	TEST_FLOAT_EQ(-11e-1, parse_float("-11e-1", 6));
+	TEST_FLOAT_EQ(-00011e-01, parse_float("-00011e-01", 10));
+	TEST_FLOAT_EQ(0.11e1, parse_float("0.11e1", 6));
 }
 #endif
